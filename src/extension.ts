c7145cc81b84dd4686e4a5c77f3013cc5c030811
Jacob Williams <jacobaw@gmail.com>
2020-07-26 21:47:48 -0700

    add commands for opening the board editor

--- src/extension.ts
+++ src/extension.ts
@@ -3,4 +3,12 @@ import { BoardEditorProvider } from './boardEditor';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(BoardEditorProvider.register(context));
+	context.subscriptions.push(vscode.commands.registerCommand('markheadboard.boardEditor.reopen', () => {
+		vscode.commands.executeCommand('vscode.openWith', vscode.window.activeTextEditor?.document.uri, 'markheadboard.boardEditor');
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('markheadboard.boardEditor.openToSide', () => {
+		vscode.window.showTextDocument(vscode.window.activeTextEditor!.document.uri, { viewColumn: vscode.ViewColumn.Beside }).then(editor => {
+			vscode.commands.executeCommand('vscode.openWith', vscode.window.activeTextEditor?.document.uri, 'markheadboard.boardEditor');
+		});
+	}));
 }
