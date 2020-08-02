bce3e609fba59d9e6f2e268ebe625e4bc785c9a9
Jacob Williams <jacobaw@gmail.com>
2020-08-01 18:16:22 -0700

    add end property when parsing

--- src/mdutil.ts
+++ src/mdutil.ts
@@ -2,29 +2,46 @@ const HEADING_REGEXP = /^(##?) +(.*)$/mg;
 
 interface Section {
   start: number;
+  end: number;
   heading: string;
   children: Section[];
 }
 
 export function parseHeadings(doc: string): Section {
-  const root: Section = { start: 0, heading: '', children: [] };
+  const root: Section = { start: 0, end: doc.length, heading: '', children: [] };
   let h1: Section | null = null;
+  let h2: Section | null = null;
 
   for (const match of doc.matchAll(HEADING_REGEXP)) {
     if (match.index === undefined) {
       continue;
     }
+    if (h2) {
+      h2.end = match.index;
+    }
     switch (match[1]) {
       case '#':
-        h1 = { start: match.index, heading: match[2], children: [] };
+        if (h1) {
+          h1.end = match.index;
+        }
+        h1 = { start: match.index, end: 0, heading: match[2], children: [] };
+        h2 = null;
         root.children.push(h1);
         break;
       case '##':
         if (h1) {
-          h1.children.push({ start: match.index, heading: match[2], children: [] });
+          h2 = { start: match.index, end: 0, heading: match[2], children: [] };
+          h1.children.push(h2);
         }
         break;
     }
+  }
+
+  if (h1) {
+    h1.end = root.end;
+  }
+  if (h2) {
+    h2.end = root.end;
   }
 
   return root;
--- src/test/suite/mdutil.test.ts
+++ src/test/suite/mdutil.test.ts
@@ -31,23 +31,26 @@ text
 foo bar baz`;
       const expected = {
         start: 0,
+        end: doc.length,
         heading: '',
         children: [
           {
             start: 0,
+            end: doc.indexOf('# Second List'),
             heading: 'First List',
             children: [
-              { start: doc.indexOf('## Card 1.1'), heading: 'Card 1.1', children: [] },
-              { start: doc.indexOf('## Card 1.2'), heading: 'Card 1.2', children: [] },
-              { start: doc.indexOf('## Card 1.3'), heading: 'Card 1.3', children: [] },
+              { start: doc.indexOf('## Card 1.1'), end: doc.indexOf('## Card 1.2'), heading: 'Card 1.1', children: [] },
+              { start: doc.indexOf('## Card 1.2'), end: doc.indexOf('## Card 1.3'), heading: 'Card 1.2', children: [] },
+              { start: doc.indexOf('## Card 1.3'), end: doc.indexOf('# Second List'), heading: 'Card 1.3', children: [] },
             ],
           },
-          { start: doc.indexOf('# Second List'), heading: 'Second List', children: [] },
+          { start: doc.indexOf('# Second List'), end: doc.indexOf('# Third List'), heading: 'Second List', children: [] },
           {
             start: doc.indexOf('# Third List'),
+            end: doc.length,
             heading: 'Third List',
             children: [
-              { start: doc.indexOf('## Card 3.1'), heading: 'Card 3.1', children: [] },
+              { start: doc.indexOf('## Card 3.1'), end: doc.length, heading: 'Card 3.1', children: [] },
             ],
           },
         ],
